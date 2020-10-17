c138eb4f5245a52a118fbd61ab1220e368b6ba67
Chirag Maniar <chirag.maniar@somaiya.edu>
2020-10-17 22:04:23 +0530

    Create send_keys.rb

---
+++ send_keys.rb
@@ -0,0 +1,18 @@
+# Here is the sample code for send_keys and saving a screenshot
+
+require 'rubygems'
+require 'selenium-webdriver'
+
+driver = Selenium::WebDriver.for :chrome
+
+driver.get 'https://stackoverflow.com/questions/10040953/press-tab-and-then-enter-key-in-selenium-webdriver-with-ruby'
+
+driver.save_screenshot("screenshot.png")
+element = driver.find_element(:id, 'top-search').click
+
+element.send_keys('ckm')
+element.send_keys(:enter)
+
+sleep(4)
+
+driver.quit
