f5b05d1a48a5c2d83ce75c8191d98b782c87ff70
mrcm123 <43877240+mrcm123@users.noreply.github.com>
2020-10-17 22:10:47 +0530

    Making findelement global variable for more robust

--- send_keys.rb
+++ send_keys.rb
@@ -7,11 +7,12 @@ require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :chrome
 URL = 'https://stackoverflow.com/questions/10040953/press-tab-and-then-enter-key-in-selenium-webdriver-with-ruby'
 SCREENSHOT_NAME = 'screenshot.png'
+SEARCH = 'top-search'
 
 driver.get URL
 
 driver.save_screenshot(SCREENSHOT_NAME) # Making global variable for saving the screenshot name
-element = driver.find_element(:id, 'top-search').click
+element = driver.find_element(:id, SEARCH).click
 
 element.send_keys('ckm')
 element.send_keys(:enter)
